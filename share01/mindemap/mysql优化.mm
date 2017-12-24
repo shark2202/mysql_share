ea60d3a00fc8c153c0f731620528f1643c67bef2
zd04 <1032869152@qq.com>
2017-12-24 22:40:04 +0800

    mysql优化脑图

---
+++ share01/mindemap/mysql优化.mm
@@ -0,0 +1,143 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="mysql&#x4f18;&#x5316;" FOLDED="false" ID="ID_469537053" CREATED="1514124258435" MODIFIED="1514124271908" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="mysql&#x7d22;&#x5f15;" FOLDED="true" POSITION="right" ID="ID_259488056" CREATED="1514124316780" MODIFIED="1514124328204">
+<edge COLOR="#ff0000"/>
+<node TEXT="&#x662f;&#x4ec0;&#x4e48;" ID="ID_1046118383" CREATED="1514124453569" MODIFIED="1514124459107">
+<node TEXT="&#x6392;&#x597d;&#x5e8f;&#x7684;&#x5feb;&#x901f;&#x67e5;&#x627e;&#x6570;&#x636e;&#x7ed3;&#x6784;" ID="ID_1581624200" CREATED="1514125036123" MODIFIED="1514125058060"/>
+</node>
+<node TEXT="&#x5206;&#x7c7b;" ID="ID_772247303" CREATED="1514124487399" MODIFIED="1514124492980">
+<node TEXT="Btree" ID="ID_108336495" CREATED="1514125096010" MODIFIED="1514125119503"/>
+<node TEXT="&#x805a;&#x96c6;&#x7d22;&#x5f15;&#xff0c;&#x6b21;&#x8981;&#x7d22;&#x5f15;&#xff0c;&#x8986;&#x76d6;&#x7d22;&#x5f15;&#x7d22;&#x5f15;&#xff0c;&#x590d;&#x5408;&#x7d22;&#x5f15;&#xff0c;&#x524d;&#x7f00;&#x7d22;&#x5f15;&#xff0c;&#x552f;&#x4e00;" ID="ID_1482010626" CREATED="1514125123091" MODIFIED="1514125204379"/>
+<node TEXT="hash&#x7d22;&#x5f15;" ID="ID_453703293" CREATED="1514125209675" MODIFIED="1514125227206"/>
+</node>
+<node TEXT="&#x4f18;&#x52a3;" ID="ID_1014369122" CREATED="1514125091416" MODIFIED="1514125238628">
+<node TEXT="&#x4f18;&#x52bf;" ID="ID_1524095368" CREATED="1514125240280" MODIFIED="1514125247796">
+<node TEXT="&#x67e5;&#x627e;" ID="ID_1027682820" CREATED="1514125264264" MODIFIED="1514125271325"/>
+<node TEXT="&#x6392;&#x5e8f;" ID="ID_774625013" CREATED="1514125272949" MODIFIED="1514125373858"/>
+</node>
+<node TEXT="&#x52a3;&#x52bf;" ID="ID_117433912" CREATED="1514125251331" MODIFIED="1514125256692">
+<node TEXT="&#x662f;&#x4e00;&#x5f20;&#x8868;&#xff0c;&#x5360;&#x7528;&#x7a7a;&#x95f4;" ID="ID_1240386927" CREATED="1514125375506" MODIFIED="1514125391579"/>
+<node TEXT="&#x66f4;&#x65b0;&#x9700;&#x8981;&#x7ef4;&#x62a4;&#x7d22;&#x5f15;" ID="ID_262981599" CREATED="1514125402513" MODIFIED="1514125424692"/>
+</node>
+</node>
+</node>
+<node TEXT="mysql&#x67b6;&#x6784;" FOLDED="true" POSITION="left" ID="ID_1571772333" CREATED="1514124372110" MODIFIED="1514124381458">
+<edge COLOR="#0000ff"/>
+<node TEXT="&#x7b80;&#x5355;&#x7248;&#x672c;" ID="ID_1774056484" CREATED="1514124419647" MODIFIED="1514124428842">
+<node TEXT="" ID="ID_333345265" CREATED="1514124497769" MODIFIED="1514124670244">
+<hook URI="mysql&#x67b6;&#x6784;&#x7b80;&#x5355;&#x7248;&#x672c;.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="&#x590d;&#x6742;&#x7248;&#x672c;" ID="ID_450514387" CREATED="1514124437344" MODIFIED="1514124447346">
+<node TEXT="" ID="ID_1026961734" CREATED="1514124683718" MODIFIED="1514124691376">
+<hook URI="mysql&#x67b6;&#x6784;&#x590d;&#x6742;&#x7248;&#x672c;.jpeg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="&#x5177;&#x4f53;&#x8bf4;&#x660e;" ID="ID_756052253" CREATED="1514124707713" MODIFIED="1514124714844">
+<node TEXT="Connectors&#xff1a;&#x4e0d;&#x540c;&#x8bed;&#x8a00;&#x4e2d;&#x4e0e; SQL &#x7684;&#x4ea4;&#x4e92;" ID="ID_117109697" CREATED="1514124724448" MODIFIED="1514124787454"/>
+<node TEXT="Management Serveices &amp; Utilities&#xff1a;&#x7cfb;&#x7edf;&#x7ba1;&#x7406;&#x548c;&#x63a7;&#x5236;&#x5de5;&#x5177;&#xa;&#xa;&#x5907;&#x4efd;&#x548c;&#x6062;&#x590d;&#x7684;&#x5b89;&#x5168;&#x6027;&#xff0c;&#x590d;&#x5236;&#xff0c;&#x96c6;&#x7fa4;&#xff0c;&#x7ba1;&#x7406;&#xff0c;&#x914d;&#x7f6e;&#xff0c;&#x8fc1;&#x79fb;&#x548c;&#x5143;&#x6570;&#x636e;&#x3002;" ID="ID_1989438388" CREATED="1514124791037" MODIFIED="1514124820923"/>
+<node TEXT="Connection Pool&#xff1a;&#x8fde;&#x63a5;&#x6c60;&#xa;&#xa;&#x8fdb;&#x884c;&#x8eab;&#x4efd;&#x9a8c;&#x8bc1;&#x3001;&#x7ebf;&#x7a0b;&#x91cd;&#x7528;&#xff0c;&#x8fde;&#x63a5;&#x9650;&#x5236;&#xff0c;&#x68c0;&#x67e5;&#x5185;&#x5b58;&#xff0c;&#x6570;&#x636e;&#x7f13;&#x5b58;&#xff1b;&#x7ba1;&#x7406;&#x7528;&#x6237;&#x7684;&#x8fde;&#x63a5;&#xff0c;&#x7ebf;&#x7a0b;&#x5904;&#x7406;&#x7b49;&#x9700;&#x8981;&#x7f13;&#x5b58;&#x7684;&#x9700;&#x6c42;&#x3002;" ID="ID_175158803" CREATED="1514124826762" MODIFIED="1514124840191"/>
+<node TEXT="SQL Interface&#xff1a;SQL &#x63a5;&#x53e3;&#xa;&#xa;&#x8fdb;&#x884c; DML&#x3001;DDL&#xff0c;&#x5b58;&#x50a8;&#x8fc7;&#x7a0b;&#x3001;&#x89c6;&#x56fe;&#x3001;&#x89e6;&#x53d1;&#x5668;&#x7b49;&#x64cd;&#x4f5c;&#x548c;&#x7ba1;&#x7406;&#xff1b;&#x7528;&#x6237;&#x901a;&#x8fc7; SQL &#x547d;&#x4ee4;&#x6765;&#x67e5;&#x8be2;&#x6240;&#x9700;&#x7ed3;&#x679c;&#x3002;" ID="ID_1206279922" CREATED="1514124873266" MODIFIED="1514124885677"/>
+<node TEXT="Parser&#xff1a;&#x89e3;&#x6790;&#x5668;&#xa;&#xa;&#x67e5;&#x8be2;&#x7ffb;&#x8bd1;&#x5bf9;&#x8c61;&#x7684;&#x7279;&#x6743;&#xff1b;SQL &#x547d;&#x4ee4;&#x4f20;&#x9012;&#x5230;&#x89e3;&#x6790;&#x5668;&#x7684;&#x65f6;&#x5019;&#x4f1a;&#x88ab;&#x89e3;&#x6790;&#x5668;&#x9a8c;&#x8bc1;&#x548c;&#x89e3;&#x6790;&#x3002;" ID="ID_463067042" CREATED="1514124897700" MODIFIED="1514124899583"/>
+<node TEXT="Optimizer&#xff1a;&#x67e5;&#x8be2;&#x4f18;&#x5316;&#x5668;" ID="ID_482688451" CREATED="1514124925830" MODIFIED="1514124927815"/>
+<node TEXT="Cache &#x548c; Buffer&#xff1a;&#x67e5;&#x8be2;&#x7f13;&#x5b58;&#xa;&#xa;&#x5168;&#x5c40;&#x548c;&#x5f15;&#x64ce;&#x7279;&#x5b9a;&#x7684;&#x7f13;&#x5b58;&#x548c;&#x7f13;&#x51b2;&#x533a;&#xff1b;" ID="ID_529237110" CREATED="1514124945112" MODIFIED="1514124947063"/>
+<node TEXT="Engine&#xff1a;&#x5b58;&#x50a8;&#x5f15;&#x64ce;" ID="ID_1643536707" CREATED="1514124957695" MODIFIED="1514124959220"/>
+</node>
+</node>
+</node>
+<node TEXT="sql&#x4f18;&#x5316;" POSITION="right" ID="ID_1250967389" CREATED="1514125621706" MODIFIED="1514125976924">
+<edge COLOR="#00ff00"/>
+<node TEXT="explain&#x5206;&#x6790;" ID="ID_286855030" CREATED="1514125979659" MODIFIED="1514125996421">
+<node TEXT="&#x662f;&#x4ec0;&#x4e48;" ID="ID_963849306" CREATED="1514126149749" MODIFIED="1514126155770">
+<node TEXT="&#x67e5;&#x770b;mysql&#x662f;&#x5982;&#x4f55;&#x5904;&#x7406;sql&#x8bed;&#x53e5;&#x7684;" ID="ID_192170215" CREATED="1514126158918" MODIFIED="1514126187950"/>
+</node>
+<node TEXT="&#x80fd;&#x505a;&#x4ec0;&#x4e48;" ID="ID_751969325" CREATED="1514126224144" MODIFIED="1514126238120">
+<node TEXT="&#x8868;&#x8bfb;&#x53d6;&#x7684;&#x987a;&#x5e8f;" ID="ID_483920743" CREATED="1514126240232" MODIFIED="1514126267251"/>
+<node TEXT="&#x6570;&#x636e;&#x8bfb;&#x53d6;&#x64cd;&#x4f5c;&#x7684;&#x64cd;&#x4f5c;&#x7c7b;&#x578b;" ID="ID_1628966181" CREATED="1514126267742" MODIFIED="1514126281492"/>
+<node TEXT="&#x90a3;&#x4e9b;&#x7d22;&#x5f15;&#x53ef;&#x4ee5;&#x4f7f;&#x7528;" ID="ID_450885846" CREATED="1514126281945" MODIFIED="1514126292059"/>
+<node TEXT="&#x5b9e;&#x9645;&#x4f7f;&#x7528;&#x4e86;&#x90a3;&#x4e9b;&#x7d22;&#x5f15;" ID="ID_1483332840" CREATED="1514126293298" MODIFIED="1514126303538"/>
+<node TEXT="&#x8868;&#x4e4b;&#x95f4;&#x7684;&#x5f15;&#x7528;&#x5173;&#x7cfb;" ID="ID_1700990415" CREATED="1514126305115" MODIFIED="1514126316506"/>
+<node TEXT="" ID="ID_520570950" CREATED="1514126317000" MODIFIED="1514126317000"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x4f18;&#x5316;&#x65b9;&#x5411;" FOLDED="true" POSITION="left" ID="ID_30557821" CREATED="1514125784818" MODIFIED="1514125791437">
+<edge COLOR="#ff00ff"/>
+<node TEXT="" ID="ID_11674684" CREATED="1514125871677" MODIFIED="1514125871678">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="&#x786c;&#x4ef6;" ID="ID_748955711" CREATED="1514125793526" MODIFIED="1514125801837"/>
+<node TEXT="&#x7cfb;&#x7edf;&#x4f18;&#x5316;" ID="ID_1617581871" CREATED="1514125806395" MODIFIED="1514125814774"/>
+<node TEXT="&#x8868;&#x7ed3;&#x6784;" ID="ID_1471912435" CREATED="1514125815119" MODIFIED="1514125822851"/>
+<node TEXT="sql&#x548c;&#x7d22;&#x5f15;" ID="ID_1819377178" CREATED="1514125824287" MODIFIED="1514125839166"/>
+<node TEXT="" ID="ID_576426041" CREATED="1514125871675" MODIFIED="1514125871677">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="&#x6210;&#x672c;&#x4ece;&#x4e0b;&#x5012;&#x4e0a;&#x9010;&#x6e10;&#x5347;&#x9ad8;&#xa;&#x6548;&#x679c;&#x4ece;&#x4e0a;&#x5230;&#x4e0b;&#x9010;&#x6e10;&#x964d;&#x4f4e;" ID="ID_1691704267" CREATED="1514125871678" MODIFIED="1514125953140"/>
+</node>
+</node>
+</node>
+</map>
